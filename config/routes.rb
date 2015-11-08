c3c6b5dfca8f28fb96ebe489e95f8c5d9758208d
ikhakoo <ikhakoo@gmail.com>
2015-11-07 19:09:13 -0500

    remove root for index

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   
   resources :clinicians
   resources :patients
-  root 'clinicians#index'
-  devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_for :users, controllers: { registrations: 'users/registrations' }
 
 end
