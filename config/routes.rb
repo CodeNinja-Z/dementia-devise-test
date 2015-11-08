87089680d6e0cf9e9cf754d6703736d98c86bcb8
ikhakoo <ikhakoo@gmail.com>
2015-11-07 19:17:00 -0500

    fixed sessions stuff

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   resources :patients
   devise_for :users, controllers: { registrations: 'users/registrations' }
 
-   root to: 'pages#index'
+  root to: 'pages#index'
 end
